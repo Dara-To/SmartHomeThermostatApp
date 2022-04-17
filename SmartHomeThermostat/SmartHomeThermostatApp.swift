1fa7b3abba244bf9ad5e64fbecb37474d11b8efa
Dara-To <33760969+Dara-To@users.noreply.github.com>
2022-04-17 18:20:17 -0400

    Initial Commit

---
+++ SmartHomeThermostat/SmartHomeThermostatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartHomeThermostatApp.swift
+//  SmartHomeThermostat
+//
+//  Created by Dara To on 2022-04-17.
+//
+
+import SwiftUI
+
+@main
+struct SmartHomeThermostatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
