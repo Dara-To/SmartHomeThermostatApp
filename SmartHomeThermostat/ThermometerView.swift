9d1c0c75d204f822bbb9e7a69b5679e8c3ff63a2
Dara-To <33760969+Dara-To@users.noreply.github.com>
2022-04-18 06:06:46 -0400

    Part 4

--- SmartHomeThermostat/ThermometerView.swift
+++ SmartHomeThermostat/ThermometerView.swift
@@ -15,9 +15,39 @@ enum Status: String {
 
 struct ThermometerView: View {
     private let ringSize: CGFloat = 220
+    private let outerDialSize: CGFloat = 200
+    private let minTemperature: CGFloat = 4
+    private let maxTemperature: CGFloat = 30
     
-    @State private var ringValue: CGFloat = 0.5
+    @State private var currentTemperature: CGFloat = 0
     @State private var degrees: CGFloat = 36
+    @State private var showStatus = false
+    
+//    @State private var x: CGFloat = 0
+//    @State private var y: CGFloat = 0
+//    @State private var angle: CGFloat = 0
+    
+    var targetTemperature: CGFloat {
+        return min(max(degrees / 360 * 40, minTemperature), maxTemperature)
+    }
+    
+    var ringValue: CGFloat {
+        return currentTemperature / 40
+    }
+    
+    var status: Status {
+        if currentTemperature < targetTemperature {
+            return .heating
+        } else if currentTemperature > targetTemperature {
+            return .cooling
+        } else {
+            return .reaching
+        }
+    }
+    
+    let timer = Timer
+        .publish(every: 1, on: .main, in: .common)
+        .autoconnect()
     
     var body: some View {
         ZStack {
@@ -38,9 +68,56 @@ struct ThermometerView: View {
             
             // MARK: Thermometer Dial
             ThermometerDialView(degrees: degrees)
+                .gesture(
+                    DragGesture()
+                        .onChanged({ value in
+                            let x = min(max(value.location.x, 0), outerDialSize)
+                            let y = min(max(value.location.y, 0), outerDialSize)
+//                            self.x = x
+//                            self.y = y
+                            
+                            let endPoint = CGPoint(x: x, y: y)
+                            let centerPoint = CGPoint(x: outerDialSize / 2, y: outerDialSize / 2)
+                            
+                            let angle = calculateAngle(centerPoint: centerPoint, endPoint: endPoint)
+                            
+                            if angle < 36 || angle > 270 { return }
+//                            self.angle = angle
+                            
+                            degrees = angle - angle.remainder(dividingBy: 9)
+                        })
+                )
             
             // MARK: Thermometer Summary
-            ThermometerSummaryView(status: .heating, showStatus: true, temperature: 22)
+            ThermometerSummaryView(status: status, showStatus: showStatus, temperature: currentTemperature)
+            
+//            VStack {
+//                Text("x: \(x), y: \(y)")
+//                Text("angle: \(angle.formatted())")
+//                Text("degrees: \(degrees.formatted())")
+//                Spacer()
+//            }
+//            .foregroundColor(.white)
+        }
+        .onAppear {
+            currentTemperature = 22
+            degrees = currentTemperature / 40 * 360
+        }
+        .onReceive(timer) { _ in
+            switch status {
+            case .heating:
+                showStatus = true
+                currentTemperature += 1
+            case .cooling:
+                showStatus = true
+                currentTemperature -= 1
+            case .reaching:
+                showStatus = false
+                break
+            }
+        }
+        .onDisappear {
+            timer.upstream.connect().cancel()
         }
     }
 }
@@ -50,5 +127,14 @@ struct ThermometerView_Previews: PreviewProvider {
         ThermometerView()
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .background(Color("Background"))
+    }
+}
+
+extension ThermometerView {
+    private func calculateAngle(centerPoint: CGPoint, endPoint: CGPoint) -> CGFloat {
+        let radians = atan2(endPoint.x - centerPoint.x, centerPoint.y - endPoint.y)
+        let degrees = 180 + (radians * 180 / .pi)
+        
+        return degrees
     }
 }
